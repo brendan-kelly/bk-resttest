76888b9bbc3f26dc235ad1ac7047215800b2a489
Brendan Kelly <brendanhkelly@gmail.com>
2017-08-21 20:12:27 -0700

    Created setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='resttest',
+    version='1.0.0',
+    description='Bench restTest',
+    author='Brendan Kelly',
+    author_email='brendanhkelly@gmail.com',
+    packages=['resttest']
+)
